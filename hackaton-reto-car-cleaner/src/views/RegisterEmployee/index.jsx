eb4345f7316325eed5512b152017c33cd31b09a8
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-11-21 14:38:10 -0600

    Cambios

--- hackaton-reto-car-cleaner/src/views/RegisterEmployee/index.jsx
+++ hackaton-reto-car-cleaner/src/views/RegisterEmployee/index.jsx
@@ -31,3 +31,42 @@ export default withFirebaseAuth({
 })(RegisterEmployee); 
 
 
+
+
+class App extends Component {   
+    render() {
+      const {
+        user,
+        signOut,
+        signInWithGoogle,
+      } = this.props;
+  
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            {
+              user
+                ? <p>Hello, {user.displayName}</p>
+                : <p>Please sign in.</p>
+            }
+  
+            {
+              user
+                ? <button onClick={signOut}>Sign out</button>
+                : <button onClick={signInWithGoogle}>Sign in with Google</button>
+            }
+          </header>
+        </div>
+      );
+    }
+  }
+  
+  const firebaseAppAuth = firebaseApp.auth();
+  
+  const providers = {
+    googleProvider: new firebase.auth.GoogleAuthProvider(),
+  };
+  
+
+
